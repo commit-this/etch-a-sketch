bc84c3a169b59035f7ccd377b8c773b7e3b250b2
Han Li <han.pecktree@gmail.com>
2023-03-07 22:18:27 -0700

    Add functions to clear existing grid and generate new grid

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-function generateDivs(gridLength) {
+function generateGrid(gridLength) {
     const container = document.querySelector(".container");
 
     for (let i = 0; i < gridLength; i++) {
-        let row = document.createElement("div");
+        const row = document.createElement("div");
         row.className = "row";
         for (let j = 0; j < gridLength; j++) {
-            let cell = document.createElement("div");
+            const cell = document.createElement("div");
             cell.className = "cell";
             row.appendChild(cell);
-            cell.textContent = "test";
+            // cell.textContent = "test";
         }
         container.appendChild(row);
     }
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => cell.addEventListener("mouseenter", changeCellColor));
 }
 
-function changeDivColor (event) {
+function changeCellColor(event) {
     const el = event.target;
     el.setAttribute("style", "background-color: red");
-    // console.log("in");
 }
 
-generateDivs(4);
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach(cell => cell.addEventListener("mouseenter", changeDivColor));
+function clearGrid() {
+    const container = document.querySelector(".container");
+    const rows = document.querySelectorAll(".rows");
+    // rows.forEach(row => removeAllChildNodes(row));
+    removeAllChildNodes(container);
+}
+
+// page will load with 16x16 grid
+generateGrid(16);
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    clearGrid();
+    const newSize = parseInt(prompt("Please input the desired grid size:"));
+    // No valid user input will generate new default size 16x16 grid
+    if (newSize === null || newSize === NaN) {
+        generateGrid(16);
+    } else {
+        generateGrid(newSize);
+    }
+});
